f22a77461155741c4d5ad833937f02977b1cbe92
umme-682 <umme.salama144@gmail.com>
2024-09-01 13:56:54 +0500

    print

---
+++ main.py
@@ -0,0 +1,8 @@
+#print a sentence 
+print("Hello Everyone")
+#print a number
+print(4)
+#print both simultaneously
+print("The number being printed", 4)
+#print an empty line before/after a sentence
+print("\nI am penguin")
